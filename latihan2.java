f5f07c8af287de3b9d9cf970db3ad3f94efde8e6
Zidan Putra Rahman <68506833+zidan-p@users.noreply.github.com>
2021-01-09 08:50:13 +0700

    Add files via upload

---
+++ latihan2.java
@@ -0,0 +1,18 @@
+
+package latihanPBO;
+
+
+public class latihan2 {
+
+
+    public static void main(String[] args) {
+        int nilai_pbo, nilai_web; // mendeklarasikan variabeo
+        nilai_pbo = 90;             // mengisi nilai variabel nilai_pbo
+        nilai_web = 80;            // mengisis nilai dari variabel nilai_web
+        System.out.println("Varianbel yang terdapat dalam program");    //menampilkan kalimat
+        System.out.println("nilai_pbo = "+nilai_pbo);   //menampilkan kalimat beserta variabel nilai_pbo
+        System.out.println("nilai_web = "+nilai_web);       //menampilkan kalimat beserta variabel nilai_web
+        
+    }
+    
+}
